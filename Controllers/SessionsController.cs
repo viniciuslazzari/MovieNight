2f97fca8db2c432a99c587b86ab89fc41c804de2
Vinícius Lazzari <vinilazzari028@gmail.com>
2022-01-12 23:47:56 -0300

    ✨ Sessions controller

--- Controllers/SessionsController.cs
+++ Controllers/SessionsController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+﻿using CinemaApi.Domain;
+using CinemaApi.Infrastructure;
+using CinemaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CinemaApi.Controllers
 {
@@ -7,31 +14,95 @@ namespace CinemaApi.Controllers
     [ApiController]
     public class SessionsController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ILogger<SessionsController> _logger;
+        private readonly SessionsRepository _sessionsRepository;
+
+        public SessionsController(ILogger<SessionsController> logger, SessionsRepository moviesRepository)
         {
-            return new string[] { "value1", "value2" };
+            _logger = logger;
+            _sessionsRepository = moviesRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+        {
+            var sessions = await _sessionsRepository.GetAll(cancellationToken);
+
+            return Ok(sessions);
         }
 
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
         {
-            return "value";
+            var session = await _sessionsRepository.GetById(id, cancellationToken);
+
+            return Ok(session);
         }
 
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] NewSessionInputModel inputModel, CancellationToken cancellationToken)
         {
+            var newSession = Session.Create(inputModel);
+            if (newSession.IsFailure)
+                return BadRequest(newSession.Error);
+
+            await _sessionsRepository.Create(newSession.Value, cancellationToken);
+            await _sessionsRepository.Commit(cancellationToken);
+
+            return CreatedAtAction("GetById", new { id = newSession.Value.Id }, newSession.Value.Id);
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(Guid id, [FromBody] UpdateSessionInputModel inputModel, CancellationToken cancellationToken)
         {
+            var oldSession = await _sessionsRepository.GetById(id, cancellationToken);
+
+            if (oldSession == null)
+                return NotFound();
+
+            // BUG ESTRANHO AQUI
+            var existingTickets =
+                oldSession.Tickets
+                    .Where(c => inputModel.Tickets.Any(input => input.Id == c.Id))
+                    .Select(c => c.Id);
+
+            var deletedTickets =
+                oldSession.Tickets
+                    .Where(c => existingTickets.Any(id => id != c.Id))
+                    .Select(c => c.Id);
+
+            oldSession.DeleteTickets(deletedTickets);
+
+            foreach (var ticket in inputModel.Tickets)
+            {
+                if (ticket.Id == Guid.Empty)
+                {
+                    oldSession.AddTicket(ticket.Client, ticket.Amount);
+                }
+                else
+                {
+                    oldSession.UpdateTicket(ticket.Id, ticket.Client, ticket.Amount);
+                }
+            }
+
+            _sessionsRepository.Update(id, inputModel, cancellationToken);
+            await _sessionsRepository.Commit(cancellationToken);
+
+            return Ok(oldSession);
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
         {
+            var removedSession = await _sessionsRepository.GetById(id, cancellationToken);
+
+            if (removedSession == null)
+                return NotFound();
+
+            _sessionsRepository.Delete(removedSession);
+            await _sessionsRepository.Commit(cancellationToken);
+
+            return Ok("Session removed successfully!");
         }
     }
 }
--- Domain/Session.cs
+++ Domain/Session.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CinemaApi.Models;
+using CSharpFunctionalExtensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace CinemaApi.Domain
 {
@@ -22,6 +25,33 @@ namespace CinemaApi.Domain
             MaxOccupation = maxOccupation;
             Price = price;
             _tickets = tickets;
+        }
+
+        public static Result<Session> Create(NewSessionInputModel inputModel)
+        {
+            var newSession = new Session(Guid.NewGuid(), inputModel.Date, inputModel.MaxOccupation, inputModel.Price, new List<Ticket>());
+
+            return newSession;
+        }
+
+        public void AddTicket(string client, int amount)
+        {
+            var newTicket = new Ticket(Guid.NewGuid(), client, amount);
+            _tickets.Add(newTicket);
+        }
+
+        public void UpdateTicket(Guid id, string client, int amount)
+        {
+            var ticket = _tickets.FirstOrDefault(item => item.Id == id);
+            if (ticket != null)
+                ticket = new Ticket(id, client, amount);
+        }
+
+        public void DeleteTickets(IEnumerable<Guid> deletedTickets)
+        {
+            var tickets = _tickets.Where(c => deletedTickets.Any(id => id == c.Id));
+            foreach (var ticket in tickets)
+                _tickets.Remove(ticket);
         }
     }
 }
